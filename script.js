76444dacf0a2d79dae15df5cf3980e200c2067a0
Singapore750 <113729042+Singapore750@users.noreply.github.com>
2023-03-04 07:07:37 -0600

    ok

--- script.js
+++ script.js
@@ -17,5 +17,20 @@ const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 camera.position.z = 5;
+camera.position.y = 2;
 
+camera.position.set(0, 2, 5);
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate() {
+box.rotation.x += 0.01;
+box.rotation.y += 0.01;
+renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
 renderer.render(scene, camera);
